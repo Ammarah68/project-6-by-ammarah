b08752b9a2c3f94ddfa8f57e273684833f40ac76
Ammarah68 <161925645+Ammarah68@users.noreply.github.com>
2025-05-15 18:59:03 +0500

    16. Function Decorators
    Assignment:
    Write a decorator function log_function_call that prints "Function is being called" before a function executes. Apply it to a function say_hello().

---
+++ function-decorators.py
@@ -0,0 +1,13 @@
+# Decorator definition
+def log_function_call(func):
+    def wrapper():
+        print("Function is being called")
+        return func()
+    return wrapper
+
+# Applying the decorator
+@log_function_call
+def say_hello():
+    print("Hello!")
+
+say_hello()
